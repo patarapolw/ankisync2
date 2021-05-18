a41580197eab7f180f02a38a4aa912eb54cfaa93
Pacharapol Withayasakpunt <polv@polv.cc>
2021-05-18 15:41:35 +0700

    use xpra instead of hostdisplay

--- anki2.1.26/start.sh
+++ anki2.1.26/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 x11docker \
-    --hostdisplay --clipboard --pulseaudio --no-entrypoint --home=$PWD/home \
+    --xpra --clipboard --pulseaudio --no-entrypoint --home=$PWD/home \
     -- \
     -- patarapolw/anki2.1.26 anki
